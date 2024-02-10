0d2f69dd27b2c49e319e50cfe0b9ee23b16de461
ldemoulins <louis.demoulins@sfr.fr>
2024-02-10 11:43:00 -0500

    Fixed the version of the paper api to make sure tests can run

--- mending-enchant-core/build.gradle.kts
+++ mending-enchant-core/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
     gradlePluginPortal()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://maven.enginehub.org/repo/")
+    maven("https://repo.papermc.io/repository/maven-public/") // MockBukkit
 }
 
 java {
@@ -34,7 +35,13 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.68.0")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.70.0")
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("io.papermc.paper:paper-api:1.20.4-R0.1-20240205.114523-90")
+    }
 }
 
 tasks.named<Test>("test") {
