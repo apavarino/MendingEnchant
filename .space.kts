750990d73aa67d2e789496e2f103fc85977c4dc6
Arthur Pavarino <5781079+apavarino@users.noreply.github.com>
2024-02-12 21:12:11 +0100

    Update CI/CD configuration

--- .space.kts
+++ .space.kts
@@ -1,7 +1,7 @@
 job("Build and publish") {
     container(displayName = "Run publish script", image = "gradle") {
         kotlinScript { api ->
-            api.gradlew("test build")
+            api.gradlew("test")
             api.gradlew("publish")
         }
     }
